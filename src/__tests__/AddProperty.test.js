f7b78bd488ad652d92e73cfd21a2ff5abd7d2dab
David Cheung <davidcheungtw@gmail.com>
2023-04-15 19:16:57 +0100

    Add post request test to surreal-estate-api

--- src/__tests__/AddProperty.test.js
+++ src/__tests__/AddProperty.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import axios from "axios";
 import AddProperty from "../components/AddProperty";
 
 describe("AddProperty", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("Renders as expected", () => {
     const rendered = renderer.create(<AddProperty />);
 
@@ -24,5 +29,33 @@ describe("AddProperty", () => {
     const buttons = screen.getAllByRole("button");
     expect(buttons).toHaveLength(1);
     expect(buttons[0]).toHaveTextContent("Add");
+  });
+
+  test("Renders post request to Surreal Estate API", async () => {
+    const mAxiosResponse = { statusText: "Created!" };
+    jest.spyOn(axios, "post").mockResolvedValue(mAxiosResponse);
+    render(<AddProperty />);
+
+    const textboxs = screen.getAllByRole("textbox");
+    const comboboxs = screen.getAllByRole("combobox");
+    const spinbutton = screen.getAllByRole("spinbutton");
+    const button = screen.getByRole("button");
+
+    fireEvent.change(textboxs[0], { target: { value: "2 bed flat" } });
+    fireEvent.change(textboxs[1], {
+      target: { value: "john.smith@email.co.uk" },
+    });
+    fireEvent.change(comboboxs[0], { target: { value: "Flat" } });
+    fireEvent.change(comboboxs[1], { target: { value: "Manchester" } });
+    fireEvent.change(spinbutton[0], { target: { value: 2 } });
+    fireEvent.change(spinbutton[1], { target: { value: 2 } });
+    fireEvent.change(spinbutton[2], { target: { value: 1230 } });
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Message: Created!/i)).toBeInstanceOf(
+        HTMLDivElement
+      );
+    });
   });
 });
